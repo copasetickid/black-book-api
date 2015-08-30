5397b9065b85c108178b49a3232f387a9f68f94c
Rushaine McBean <rushaine.mcbean@gmail.com>
2015-08-29 20:03:23 -0400

    Add in controller actions & tests for people resource

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,16 +1,49 @@
 class PeopleController < ApplicationController
+  before_action :set_person, only: [ :show, :update, :destroy]
+
   def index
+    @people = Person.all
+
+    render json: @people
   end
 
   def show
+    render json: @person
   end
 
   def create
+    @person = Person.new(person_params)
+
+    if @person.save
+      render json: @person, status: :created, location: @person
+    else
+      render json: @person.errors, status: :unprocessable_entity
+    end
   end
 
   def update
+
+    if @person.update(person_params)
+      head :no_content
+    else
+      render json: @person.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+
+    @person.destroy
+
+    head :no_content
+  end
+
+  private
+
+  def set_person
+    @person = Person.find(params[:id])
+  end
+
+  def person_params
+      params.require(:person).permit(:full_name, :email, :phone)
   end
 end
--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -2,10 +2,95 @@ require 'rails_helper'
 
 RSpec.describe PeopleController, type: :controller do
 
-  it { expect(get:    "/people").to   route_to("people#index") }
-  it { expect(get:    "/people/1").to route_to("people#show", id: "1") }
-  it { expect(post:   "/people").to   route_to("people#create") }
-  it { expect(put:    "/people/1").to route_to("people#update", id: "1") }
-  it { expect(delete: "/people/1").to route_to("people#destroy", id: "1") }
+  let!(:person) { create(:person)}
 
+
+  describe "GET #index" do
+    it "assigns all people as @people" do
+      get :index, { format: :json }
+      expect(assigns(:people)).to include(person)
+    end
+  end
+
+  describe "GET #show" do
+    it "assigns the requested person as @person" do
+      get :show, { id: person.id, format: :json }
+      expect(assigns(:person)).to eq person
+    end
+  end
+
+  describe "POST #create" do
+
+    context "with valid params" do
+      let(:person_attrs) { attributes_for(:person)}
+
+      it "creates a new Person" do
+        expect {
+          post :create, { person: person_attrs, format: :json  }
+        }.to change{Person.count}.by(1)
+      end
+
+      it "assigns a newly created person as @person" do
+        post :create, { person: person_attrs, format: :json  }
+        expect(assigns(:person)).to be_a(Person)
+        expect(assigns(:person)).to be_persisted
+      end
+    end
+
+    context "with invalid params" do
+
+
+      it "assigns a newly created but unsaved person as @person" do
+
+        post :create, { person: { phone: "1234567890" }, format: :json  }
+        expect(assigns(:person)).to be_a_new(Person)
+      end
+
+      it "returns unprocessable_entity status" do
+        post :create, { person: { phone: "1234567890" }, format: :json  }
+        expect(response.status).to eq 422
+      end
+    end
+  end
+
+  describe "PUT #update" do
+    context "with valid params" do
+
+      it "updates the requested person" do
+        put :update, { id: person.id, person: { full_name: "John F. Doe"}, format: :json  }
+        person.reload
+        expect(person.full_name).to eq "John F. Doe"
+      end
+
+      it "assigns the requested person as @person" do
+        put :update, { id: person.id, person: { full_name: "John F. Doe"}, format: :json  }
+        expect(assigns(:person)).to eq person
+      end
+    end
+
+    context "with invalid person params" do
+      it "assigns the person omer as @person " do
+        put :update, { id: person .id, person: { full_name: nil} , format: :json  }
+        expect(assigns(:person)).to eq person
+      end
+
+      it "returns unprocessable_entity status" do
+        put :update, { id: person.id, person: { full_name: nil } , format: :json }
+        expect(response.status).to eq 422
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "destroys the requested person" do
+      expect {
+        delete :destroy, { id: person.id, format: :json  }
+      }.to change{Person.count}.by(-1)
+    end
+
+    it "redirects to the peoples list" do
+      delete :destroy, { id: person.id, format: :json  }
+      expect(response.status).to eq 204
+    end
+  end
 end
