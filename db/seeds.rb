9669e2adaa698b8a10bf2c65a5712adcf1cd6c19
Rushaine McBean <rushaine.mcbean@gmail.com>
2015-08-29 20:12:31 -0400

    Create sample people

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+[
+  { full_name: "Tori Kelly",   email: "tori.kelly@example.com",   phone: "033 1234 5678"},
+  { full_name: "Britney Spears", email: "britney.spearsth@example.com", phone: "034 6789 1234"},
+  { full_name: "Chris Brown",  email: "chris.brown@example.com",  phone: "033 4321 9876"},
+  { full_name: "Jesse McCartney", email: "jesse.mccartney@example.com", phone: "034 9876 1234"},
+  { full_name: "Miley Cyrus",   email: "miley.cyrus@example.com",   phone: "033 6789 4321"}
+].each do |person_attributes|
+  Person.create(person_attributes)
+end
