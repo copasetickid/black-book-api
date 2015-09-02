371bc3dc680371be40ae20de185f815a1270446f
Rushaine McBean <rushaine.mcbean@gmail.com>
2015-09-02 14:48:48 -0400

    Adds in a sessions controller for a user to login and receive a valid token

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:create, :destroy]
   resources :people, only: [:index, :show, :create, :update, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
