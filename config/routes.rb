ed289e8f374e07ec37104866fc7c34ef225bde3c
Rushaine McBean <rushaine.mcbean@gmail.com>
2015-09-04 22:29:37 -0400

    Catch all route to respond HTTP requests with CORS headers

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,21 @@ Rails.application.routes.draw do
   resources :sessions, only: [:create, :destroy]
   resources :people, only: [:index, :show, :create, :update, :destroy]
 
+
+  match "/*path",
+    to: proc {
+      [
+        204,
+        {
+          "Content-Type"                 => "text/plain",
+          "Access-Control-Allow-Origin"  => CORS_ALLOW_ORIGIN,
+          "Access-Control-Allow-Methods" => CORS_ALLOW_METHODS,
+          "Access-Control-Allow-Headers" => CORS_ALLOW_HEADERS
+        },
+        []
+      ]
+    }, via: [:options, :head]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
